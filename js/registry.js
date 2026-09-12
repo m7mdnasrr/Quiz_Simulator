854000bb54104512fde6afa9e77613a33f01fcb5
Mohamed Nasr <74052984+m7mdnasrr@users.noreply.github.com>
2026-09-13 01:04:14 +0300

    Update registry.js

--- js/registry.js
+++ js/registry.js
@@ -82,7 +82,7 @@
         'Provisioning & Scripts',
         'Security Fabric & Integrations'
       ],
-      imageDir:        'images-fmgr',
+      imageDir:        'images/fmgr',
       totalQuestions:  45,
       durationSeconds: 90 * 60,
       passPercent:     80,
@@ -107,8 +107,8 @@
         'Integrations & Fabric'
       ],
       imageDir:        'images/fsiem',
-      totalQuestions:  45,
-      durationSeconds: 90 * 60,
+      totalQuestions:  40,
+      durationSeconds: 70 * 60,
       passPercent:     80,
       available:       true,        // reserved; bank stub means card shows "Coming Soon"
       bank:            []
